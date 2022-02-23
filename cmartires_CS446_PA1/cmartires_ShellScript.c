e03771bc8b1af31f8071d0037c20063c93244bb0
martires-colin <cmartires@nevada.unr.edu>
2022-02-23 15:51:15 -0800

    file stuff

--- cmartires_CS446_PA1/cmartires_ShellScript.c
+++ cmartires_CS446_PA1/cmartires_ShellScript.c
@@ -28,25 +28,46 @@ void changeDirectories(char *tokens[], int numTokens);
 int main(int argc, char *argv[])
 {
 	bool batchmode;
+	FILE *outputFS = NULL;
+	FILE *inputFS = NULL;
+	FILE *redirectionFS = NULL;
 
-	if(argc == 1)
+	if(argc == 1)									//interactive mode
 	{
-		//interactive mode
 		batchmode = false;
+		promptUser(batchmode);
+
+
 	}
-	else if(argc == 2)
+	else if(argc == 2)								//batch mode
 	{
-		//batch mode
 		batchmode = true;
+		if(inputFS = fopen(argv[1], "r"))			//check if file exists
+		{
+			//test filestream
+			char lines[150];
+			while(!feof(inputFS))
+			{
+				fgets(lines, 150, inputFS);
+				puts(lines);
+			}	
+		}
+		else										//file doesn't exist
+		{
+			printError();
+			return 1;
+		}
 	}
-	else
+	else											//error, too many arguments
 	{
-		//error
 		printError();
-		return 0;
+		return 1;
 	}
 
-	promptUser(batchmode);
+
+
+
+
 
 
 	return 0;
@@ -61,6 +82,7 @@ void promptUser(bool isBatch)
 		gethostname(host, HOST_NAME_MAX + 1);
 		getcwd(cwd, sizeof(cwd));
 		printf("%s@%s:%s$ ", user, host, cwd);
+
 	}
 }
 
