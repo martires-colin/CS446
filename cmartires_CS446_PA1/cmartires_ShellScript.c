a13805bff908263c4e3e6ee4d5249c1d1fdd859c
martires-colin <cmartires@nevada.unr.edu>
2022-02-24 00:10:52 -0800

    implemented exit function, other stuff too

--- cmartires_CS446_PA1/cmartires_ShellScript.c
+++ cmartires_CS446_PA1/cmartires_ShellScript.c
@@ -2,6 +2,10 @@
 //Author: Colin Martires
 //Purpose: Programming Assignment 1
 
+//TODO:
+//input stream file pointer needs to be set equal to input from terminal - potentially use fgets with stdin?
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -19,7 +23,7 @@ void promptUser(bool isBatch);
 void printError();
 int parseInput(char *input, char *splitwords[]);
 char *redirectCommand(char *special, char *line, bool *isRedirect, char *tokens[], char *outputTokens[]);
-char *executeCommand(char *cmd, bool *isRedirect, char* tokens[], char* outputTokens[], bool *isExits);
+//char *executeCommand(char *cmd, bool *isRedirect, char* tokens[], char* outputTokens[], bool *isExits);
 char getLetter(char *str, int index);
 bool exitProgram(char *tokens[], int numTokens);
 void launchProcesses(char *tokens[], int numTokens, bool isRedirect);
@@ -33,20 +37,48 @@ int main(int argc, char *argv[])
 	FILE *redirectionFS = NULL;
 
 	char* parsed[100];
+	char userCmds[30];
 	int numArgs;
+	bool *isRe;
 
-	if(argc == 1)									//interactive mode
+	if(argc == 1)										//interactive mode
 	{
 		batchmode = false;
 		promptUser(batchmode);
-		//input stream file pointer needs to be set equal to input from terminal
+		fgets(userCmds, 30, stdin);						//read in user's commands
+		numArgs = parseInput(userCmds, parsed);
+		
+		/*	testing block
+		printf("entered commands: %s\n", userCmds);
+
+		for(int i = 0; i < numArgs; i++)
+		{
+			printf("%s\n", parsed[i]);
+		}
+		printf("\n");
+		printf("1st token: %s\n", parsed[0]);
+		//input stream file pointer needs to be set equal to input from terminal - potentially use fgets with stdin?
+		
+		//executeCommand(">", isRe);
+		// if(*isRe == true)
+		// {
+		// 	printf("True");
+		// }
+		// else
+		// {
+		// 	printf("False");
+		// }
+
+		exitProgram(parsed, numArgs);
+
+
+		*/	//end testing block
 	}
-	else if(argc == 2)								//batch mode
+	else if(argc == 2)									//batch mode
 	{
 		batchmode = true;
-		if(inputFS = fopen(argv[1], "r"))			//check if file exists
+		if(inputFS = fopen(argv[1], "r"))				//check if file exists
 		{
-			//test filestream
 			char lines[150];
 			while(!feof(inputFS))
 			{
@@ -60,19 +92,17 @@ int main(int argc, char *argv[])
 			}
 
 		}
-		else										//file doesn't exist
+		else											//file doesn't exist
 		{
 			printError();
 			return 1;
 		}
 	}
-	else											//error, too many arguments
+	else
 	{
-		printError();
+		printError();									//error, too many arguments
 		return 1;
 	}
-
-
 
 
 	return 0;
@@ -108,10 +138,56 @@ int parseInput(char *input, char *splitWords[])
 	return wordInd;
 }
 
-char *executeCommand(char *cmd, bool *isRedirect, char* tokens[], char* outputTokens[], bool *isExits)
+// char *executeCommand(char *cmd, bool *isRedirect, char* tokens[], char* outputTokens[], bool *isExits)
+// {
+// 	char *outFileName = "";
+
+// 	char *cmdDup = strdup(cmd);
+// 	strcat(cmdDup, "\n");
+
+// 	//test strdup
+// 	//printf("%s", cmdDup);
+
+// 	if(strchr(cmdDup, '>') != NULL)						//check if command is Redirect
+// 	{
+// 		*isRedirect = true;
+// 		//call redirectCommand
+// 	}
+// 	else
+// 	{
+// 		*isRedirect = false;
+// 		//call exitProgram
+// 		//return the output file name
+// 	}
+// 	//otherwise, call changeDirectories, printHelp, and launchProcesses, and return the output file name
+
+
+
+
+// }
+
+bool exitProgram(char *tokens[], int numTokens)
 {
-	
+	if(numTokens == 1)
+	{
+		if(strcmp(tokens[0], "exit\n") == 0)
+		{
+			printf("you typed exit!\n");
+			return true;
+		}
+		else
+		{
+			printf("you didn't type exit :(\n");
+			return false;
+		}
+	}
+	else
+	{
+		printError();										//error, too many arguments
+		return false;
+	}
 }
+
 
 
 
