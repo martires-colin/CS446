89257dfc101b2b610625e98dbcc3c691bb2cc10c
martires-colin <cmartires@nevada.unr.edu>
2022-03-03 13:41:43 -0800

    fixed python thing

--- cmartires_CS446_PA1/cmartires_examine_system.py
+++ cmartires_CS446_PA1/cmartires_examine_system.py
@@ -2,6 +2,8 @@
 #Author: Colin Martires
 #Purpose: Programming Assignment 1
 
+import subprocess
+
 f = open("cmartires_systemDetails.txt", "w")
 f.close()
 
@@ -16,10 +18,25 @@ def rwData(location, description):
 	f.write("\n")
 	f.close()
 
+def getBootTime():
+	f = open("cmartires_systemDetails.txt", "a")
+	f.write("4. Time that the system was last booted" + "\n")
+	f.close()
+	cmd = ['uptime', '-s']
+	output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+	f = open("cmartires_systemDetails.txt", "ab")
+	f.write(output)
+	f.close()
+	f = open("cmartires_systemDetails.txt", "a")
+	f.write("\n")
+	f.close()
+
 
 rwData("/proc/cpuinfo", "1. CPU type and model:")
 rwData("/proc/version", "2. Kernel version details:")
 rwData("/proc/uptime", "3. Amount of time since last boot:")
-rwData("/proc/stat", "4. Time that the system was last booted (see 'btime'):")
+getBootTime()
 rwData("/proc/diskstats", "5. Number of disk requests made:")
 rwData("/proc/stat", "6. Number of processes created since last boot (see 'processes'):")
+
+
