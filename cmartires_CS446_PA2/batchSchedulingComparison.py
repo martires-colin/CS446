7a70e7add62238d9c4b75a5814efed2d4adb1669
martires-colin <cmartires@nevada.unr.edu>
2022-03-15 20:48:41 -0700

    pa 2 start

---
+++ cmartires_CS446_PA2/batchSchedulingComparison.py
@@ -0,0 +1,7 @@
+#Program Name: batchSchedulingComparison.py
+#Author: Colin Martires
+#Purpose: Programming Assignment 2
+
+#Compare and contrast the two algorithms. Explain where each would be
+#appropriate and any possible tradeoffs in implementation or process execution
+
