ce933883e15ff867d0fd03302525e34fc75d8386
martires-colin <cmartires@nevada.unr.edu>
2022-03-18 19:50:22 -0700

    FCFS implementation

--- cmartires_CS446_PA2/batchSchedulingComparison.py
+++ cmartires_CS446_PA2/batchSchedulingComparison.py
@@ -8,6 +8,48 @@
 import sys
 import os.path
 
+#def AverageTurnaround(processCompletionTimes, processArrivalTimes):
+	
+#class to store proces information
+class Process:
+	def __init__(self, PID, arrivalTime, burstTime, priority):
+		self.PID = PID
+		self.arrivalTime = arrivalTime
+		self.burstTime = burstTime
+		self.priority = priority
+	
+	def __repr__(self):
+		return '[' + str(self.PID) + ', ' + str(self.arrivalTime) + ', ' + str(self.burstTime) + ', ' + str(self.priority) + ']'
+
+def FirstComeFirstServedSort(batchFileData):
+	processes = []
+	for x in batchFileData:
+		tokens = x.split(', ')
+		tokens[3] = tokens[3].replace('\n', '')
+		processes.append(Process(int(tokens[0]), int(tokens[1]), int(tokens[2]), int(tokens[3])))
+	
+	#print(processes)
+	sortedProcesses = sorted(processes, key = lambda z: (z.arrivalTime, z.PID))
+	
+	PIDlist = []
+	for i in sortedProcesses:
+		PIDlist.append(i.PID)
+
+
+	completionTime = 0
+	completionTimeList = []
+	for y in sortedProcesses:
+		completionTime += y.burstTime
+		completionTimeList.append(completionTime)
+
+	# print(sortedProcesses)
+	# print(PIDlist)
+	# print(completionTimeList)
+
+	return PIDlist, completionTimeList
+
+
+
 def main():
 
 	#check for correct number of arguments
@@ -29,22 +71,22 @@ def main():
 	batchFile = open(sys.argv[1], 'r')
 	data = batchFile.readlines()
 	batchFile.close()
-	#print(data)		#check data read from file
+	#print(data)		#check data read from file				PID, Arrival Time, Burst Time, Priority
 
 	#execute sorting algorithms
 	if(sys.argv[2] == 'FCFS'):
 		#FCFS stuff
 		print('You\'ve chosen FCFS')
+		PIDs, CompletionTimes = FirstComeFirstServedSort(data)
+		print(PIDs)					#checking results	
+		print(CompletionTimes)		#checking results
+
 	elif(sys.argv[2] == 'ShortestFirst'):
 		#ShortestFirst stuff
 		print('You\'ve chosen ShortestFirst')
 	elif(sys.argv[2] == 'Priority'):
 		#Priority stuff
 		print('You\'ve chosen Priority')
-
-
-
-
 
 if __name__ == '__main__':
 	main()
--- cmartires_CS446_PA2/testing/sortTest.py
+++ cmartires_CS446_PA2/testing/sortTest.py
@@ -1,23 +1,67 @@
 #practice with python
 
-class Car:
-	def __init__(self, make, model, coolness):
-		self.make = make
-		self.model = model
-		self.coolness = coolness
+# class Car:
+# 	def __init__(self, make, model, coolness):
+# 		self.make = make
+# 		self.model = model
+# 		self.coolness = coolness
+	
+# 	def __repr__(self):
+# 		return "Make: " + self.make + " Model: " + self.model + " Coolness: " + str(self.coolness)
+
+
+
+
+
+
+class Process:
+	def __init__(self, PID, arrivalTime, burstTime, priority):
+		self.PID = PID
+		self.arrivalTime = arrivalTime
+		self.burstTime = burstTime
+		self.priority = priority
 	
 	def __repr__(self):
-		return "Make: " + self.make + " Model: " + self.model + " Coolness: " + str(self.coolness)
+		return '[' + str(self.PID) + ', ' + str(self.arrivalTime) + ', ' + str(self.burstTime) + ', ' + str(self.priority) + ']'
 
 def main():
-	dreamCars = [Car("BMW", "E30", 10), Car("Datsun", "240z", 8.5), Car("Aston Martin", "DBS Superleggera", 9)]
+	# dreamCars = [Car("BMW", "E30", 10), Car("Datsun", "240z", 8.5), Car("Aston Martin", "DBS Superleggera", 9)]
 	
-	print('Unsorted')
-	print(dreamCars)
+	# print('Unsorted')
+	# print(dreamCars)
 
-	sortedList = sorted(dreamCars, key = lambda x: x.coolness)
-	print('\nSorted')
-	print(sortedList)
+	# sortedList = sorted(dreamCars, key = lambda x: x.coolness)
+	# print('\nSorted')
+	# print(sortedList)
+
+	batchFile = open('pa2_batchfile.txt', 'r')
+	data = batchFile.readlines()
+	batchFile.close()
+
+#-------start of function----passing in 'data'---#
+	#print(len(data))
+	#print(data)
+
+	processes = []
+
+	for x in data:
+		tokens = x.split(', ')
+		tokens[3] = tokens[3].replace('\n', '')
+		#print(tokens)
+		#print(tokens[3])
+
+		processes.append(Process(int(tokens[0]), int(tokens[1]), int(tokens[2]), int(tokens[3])))
+
+	print(processes)
+	sortedProcesses = sorted(processes, key = lambda z: z.priority)
+	print(sortedProcesses)
+
+	for y in processes:
+		print(y.arrivalTime)
+	#sortedList = sorted(data)
+	#print(sortedList)
+	
+
 
 if __name__ == "__main__":
 	main()
