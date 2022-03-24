68355efdc56065a35d7a421cb18c003259d3229d
martires-colin <cmartires@nevada.unr.edu>
2022-03-23 21:55:35 -0700

    final

--- cmartires_CS446_PA2/batchSchedulingComparison.py
+++ cmartires_CS446_PA2/batchSchedulingComparison.py
@@ -1,16 +1,45 @@
 #Program Name: batchSchedulingComparison.py
 #Author: Colin Martires
 #Purpose: Programming Assignment 2
+#This program implements process scheduling algorithms, specifically FCFS, SJF, and Priority Sorts.
+#The program reads data from a file containing process information and calculates the 
+#average wait time and turnaround for the selected algorithm.
 
 #Compare and contrast the three algorithms. Explain where each would be
-#appropriate and any possible tradeoffs in implementation or process execution
+#appropriate and any possible tradeoffs in implementation or process execution.
+
+# The FirstComeFirstServed sort executes processes based on their arrival time.
+# If more than one process arrives at the same time, the process with the smaller
+# PID will be executed first. This sort is non preemptive, which means that an executed
+# process will not be interrupted once it starts executing. The other non preemptive
+# sort in this program is the Priority sort, therefore, its implementation is similar
+# to the FirstComeFirstServed sort. The only difference between the FirstComeFirstServed
+# sort and Priority sort is the way each process is sorted. While processes are still
+# executed by arrival time in the Priority sort, if mutliple processes arrive at the
+# same time, instead of executing the process with a lower PID, the scheduler will
+# execute the process with higher priority. The sorting algorithm that is the most
+# different is the ShortestJobFirst sort since we are using a preemptive implementation.
+# This means that if a process arrives with a shorter burst time, the current process
+# will be stopped and the new process will start to execute.
+# When all three sorting algorithms ran with the data from "pa2_batchfile.txt", I found
+# that the ShortestJobFirst algorithm was the most efficient, with FirstComeFirstServed
+# sort coming in second, and Priority sort finishing last.
+# Both FirstComeFirstServed and ShortestJobFirst sorts are scheduling algorithms used
+# in Batch systems. You would use these sorts if you wanted to maximize throughput, keep
+# the CPU busy at all times, and minimize the turnaround time for each process. Payroll
+# and inventory systems are examples of when FirstComeFirstServed and ShortestJobFirst
+# would be used as a scheduling algorithm. On the other hand, Priority sort is a
+# scheduling technique used in interactive systems. In these types of systems, response
+# time and proportionality are the main goals. The system should respond to requests quickly
+# and satisfy the users' demands. Some examples of interactive systems that would run
+# priority scheduling are PCs and servers.
 
 import sys
 import os.path
 import operator
 from collections import deque
 
-#class to store proces information
+#class to store process information
 class Process:
 	def __init__(self, PID, arrivalTime, burstTime, priority):
 		self.PID = PID
@@ -58,10 +87,6 @@ def FirstComeFirstServedSort(batchFileData):
 		completionTime += i.burstTime
 		completionTimeList.append(completionTime)
 
-	# print(sortedProcesses)
-	# print(PIDlist)
-	# print(completionTimeList)
-
 	return PIDlist, completionTimeList, ArrivalTimeList, BurstTimeList
 
 def ShortestJobFirstSort(batchFileData):
@@ -102,12 +127,6 @@ def ShortestJobFirstSort(batchFileData):
 
 		time += 1
 
-	# print(PIDlist)
-	# print(ArrivalTimeList)
-	# print(completionTimeList)
-	# print(BurstTimeList)
-
-
 	return PIDlist, completionTimeList, ArrivalTimeList, BurstTimeList
 
 def PrioritySort(batchFileData):
@@ -125,10 +144,6 @@ def PrioritySort(batchFileData):
 		BurstTimeList.append(i.burstTime)
 		completionTime += i.burstTime
 		completionTimeList.append(completionTime)
-
-	# print(sortedProcesses)
-	# print(PIDlist)
-	# print(completionTimeList)
 
 	return PIDlist, completionTimeList, ArrivalTimeList, BurstTimeList
 
@@ -156,15 +171,8 @@ def main():
 	#EXECUTE SORTING ALGORITHMS
 	if(sys.argv[2] == 'FCFS'):
 		PIDs, CompletionTimes, ArrivalTimes, BurstTimes = FirstComeFirstServedSort(data)
-		
 		avgTurnaroundTime, TurnaroundTimes = AverageTurnaround(CompletionTimes, ArrivalTimes)
 		avgWaitTime = AverageWait(TurnaroundTimes, BurstTimes)
-
-		# print('PIDs:',PIDs)								#checking results(remove after)
-		# print('Completion Times:', CompletionTimes)		#checking results
-		# print('Arrival Times:', ArrivalTimes)			#checking results
-		# print('Burst Times:', BurstTimes)				#checking results
-		# print('Turnaround Times:', TurnaroundTimes)		#checking results
 
 		print('FCFS Sort Statistics:')
 		print('PID ORDER OF EXECUTION')
@@ -175,15 +183,8 @@ def main():
 
 	elif(sys.argv[2] == 'ShortestFirst'):
 		PIDs, CompletionTimes, ArrivalTimes, BurstTimes = ShortestJobFirstSort(data)
-
 		avgTurnaroundTime, TurnaroundTimes = AverageTurnaround(CompletionTimes, ArrivalTimes)
 		avgWaitTime = AverageWait(TurnaroundTimes, BurstTimes)
-		
-		# print('PIDs:',PIDs)								#checking results(remove after)
-		# print('Completion Times:', CompletionTimes)		#checking results
-		# print('Arrival Times:', ArrivalTimes)			#checking results
-		# print('Burst Times:', BurstTimes)				#checking results
-		# print('Turnaround Times:', TurnaroundTimes)		#checking results
 
 		print('Shortest Job First Sort Statistics:')
 		print('PID ORDER OF EXECUTION')
@@ -194,15 +195,8 @@ def main():
 
 	elif(sys.argv[2] == 'Priority'):
 		PIDs, CompletionTimes, ArrivalTimes, BurstTimes = PrioritySort(data)
-		
 		avgTurnaroundTime, TurnaroundTimes = AverageTurnaround(CompletionTimes, ArrivalTimes)
 		avgWaitTime = AverageWait(TurnaroundTimes, BurstTimes)
-
-		# print('PIDs:',PIDs)								#checking results(remove after)
-		# print('Completion Times:', CompletionTimes)		#checking results
-		# print('Arrival Times:', ArrivalTimes)			#checking results
-		# print('Burst Times:', BurstTimes)				#checking results
-		# print('Turnaround Times:', TurnaroundTimes)		#checking results
 
 		print('Priority Sort Statistics:')
 		print('PID ORDER OF EXECUTION')
