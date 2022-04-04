782aacf4a173c30a23e067d398864f3bf7766232
martires-colin <cmartires@nevada.unr.edu>
2022-04-03 20:08:59 -0700

    createDirectories now populates directorie with files

--- cmartires_CS446_PA3/fileSystemComparison.py
+++ cmartires_CS446_PA3/fileSystemComparison.py
@@ -5,28 +5,34 @@
 #directories. The program will determine the size difference as well as the
 #traversal time between the two types of directories.
 
+# COMPARE THE THINGIES
+
 # To Do
 # move files into directories in hierarchicalRoot
 
 import os
 import shutil
 
-def generateFiles(path, numOfFiles):
+def generateFiles(path, numOfFiles, start):
 	os.chdir(path)
-	ctr = 0
-	for i in range(numOfFiles):
+	for i in range(start, numOfFiles):
 		with open('file' + str(i + 1) + '.txt', 'w'):
 			pass
-		ctr += 1
-	return ctr
 
 def createDirectories(path, numDirectories):
 	os.chdir(path)
 	offset = 0
+	fileCtr = 10
+	start = 0
 	for i in range(numDirectories):
 		directoryName = 'files' + (str)(offset + 1) + '-' + (str)(offset + 10) 
 		os.makedirs(directoryName, exist_ok=True)
+		os.chdir(directoryName)
+		generateFiles(path + '/' + directoryName, fileCtr, start)
 		offset += 10
+		fileCtr += 10
+		start += 10
+		os.chdir(path)
 
 def moveToDirectory():
 	pass
@@ -36,9 +42,8 @@ def main():
 	os.makedirs('/home/cmartires/singleRoot', exist_ok=True)			#create singleRoot directory
 	os.makedirs('/home/cmartires/hierarchicalRoot', exist_ok=True)		#create hierarchicalRoot directory
 
-	numSRfiles = generateFiles('/home/cmartires/singleRoot', 100)		#generate 100 files in singleRoot
-	numHRfiles = generateFiles('/home/cmartires/hierarchicalRoot', 100)	#generate 100 files in heirarchicalRoot
-	createDirectories('/home/cmartires/hierarchicalRoot', 10)				#generate directories within hierarchicalRoot
+	generateFiles('/home/cmartires/singleRoot', 100, 0)					#generate 100 files in singleRoot
+	createDirectories('/home/cmartires/hierarchicalRoot', 10)			#generate directories?files within hierarchicalRoot
 
 
 if __name__ == '__main__':
