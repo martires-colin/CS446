e3654f44eb0225ec0fb39c96d830e65ac31e8d8f
martires-colin <cmartires@nevada.unr.edu>
2022-04-04 23:21:48 -0700

    implemented traverseDirectory, writeDataFile, and calculateAvgSize

--- cmartires_CS446_PA3/fileSystemComparison.py
+++ cmartires_CS446_PA3/fileSystemComparison.py
@@ -8,18 +8,20 @@
 # COMPARE THE THINGIES
 
 # To Do
-# move files into directories in hierarchicalRoot
+#
 
 import os
-import shutil
 
 def generateFiles(path, numOfFiles, start):
+	origin = os.getcwd()
 	os.chdir(path)
 	for i in range(start, numOfFiles):
 		with open('file' + str(i + 1) + '.txt', 'w'):
 			pass
+	os.chdir(origin)
 
 def createDirectories(path, numDirectories):
+	origin = os.getcwd()
 	os.chdir(path)
 	offset = 0
 	fileCtr = 10
@@ -33,9 +35,46 @@ def createDirectories(path, numDirectories):
 		fileCtr += 10
 		start += 10
 		os.chdir(path)
+	os.chdir(origin)
 
-def moveToDirectory():
-	pass
+def traverseDirectory(filePath):
+	dirInfo = {}
+
+	dirs = next(os.walk(filePath))[1]
+	for dirName in dirs:
+		fileSize = os.path.getsize(filePath + '/' + dirName)
+		# print(dirName, '-', fileSize)
+		dirInfo[dirName] = fileSize
+	
+		files = next(os.walk(filePath + '/' + dirName))[2]
+		for fileName in files:
+			fileSize = os.path.getsize(filePath + '/' + dirName + '/' + fileName)
+			# print(fileName, '-', fileSize)
+			dirInfo[fileName] = fileSize
+
+	files = next(os.walk(filePath))[2]
+	for fileName in files:
+		fileSize = os.path.getsize(filePath + '/' + fileName)
+		# print(fileName, '-', fileSize)
+		dirInfo[fileName] = fileSize
+
+	return dirInfo
+
+def writeDataFile(data, fileName, destination):
+	origin = os.getcwd()
+	os.chdir(destination)
+	f = open(fileName, "w")
+	for key, value in data.items():
+		f.write('File Name: %s\nFile Size: %s bytes\n\n' % (key,value))
+	f.close()
+	os.chdir(origin)
+	return len(data)
+
+def calculateAvgSize(data):
+	size = 0
+	for key, value in data.items():
+		size += value
+	return size / len(data)
 
 def main():
 
@@ -44,6 +83,20 @@ def main():
 
 	generateFiles('/home/cmartires/singleRoot', 100, 0)					#generate 100 files in singleRoot
 	createDirectories('/home/cmartires/hierarchicalRoot', 10)			#generate directories?files within hierarchicalRoot
+
+	SRdata = traverseDirectory('/home/cmartires/singleRoot')			#traverse directories
+	HRdata = traverseDirectory('/home/cmartires/hierarchicalRoot')
+
+	numSRfiles = writeDataFile(SRdata, 'singleLevelFiles.txt', '/home/cmartires/singleRoot')						#write files with directory info
+	numHRfiles = writeDataFile(HRdata, 'hierarchicalFiles.txt', '/home/cmartires/hierarchicalRoot')
+
+	avgSizeSR = calculateAvgSize(SRdata)
+	avgSizeHR = calculateAvgSize(HRdata)
+
+	print('Number of files in singleRoot:', numSRfiles)
+	print('Average File Size in singleRoot: %0.2f' % avgSizeSR)
+	print('Number of files in hierarchicalRoot:', numHRfiles)
+	print('Average File Size in hierarchicalRoot: %0.2f' % avgSizeHR)
 
 
 if __name__ == '__main__':
