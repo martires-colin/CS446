a000132e8c2998f74c42fb59ae90aca97717927e
martires-colin <cmartires@nevada.unr.edu>
2022-04-26 22:31:28 -0700

    generates ssl certificate

--- cmartires_CS446_PA4/pythonCode2.py
+++ cmartires_CS446_PA4/pythonCode2.py
@@ -5,6 +5,14 @@ import csv
 from scipy import constants
 import numpy as np
 
+from OpenSSL import crypto, SSL
+from socket import gethostname
+from pprint import pprint
+from time import gmtime, mktime
+from os.path import exists, join
+
+import os
+
 color = 128 * np.ones(shape=[3], dtype=np.uint8)
 tuplevals = tuple(color)
 im = Image.new('RGB', (512, 512), tuplevals)
@@ -18,4 +26,35 @@ try:
         reader = csv.DictReader(csvfile)
 except:
     pass
+
+# CN = "cmartires_selfSignedCertificate.PEM"
+# CN = gethostname()
+CERT_FILE = "cmartires_selfSignedCertificate.PEM"
+KEY_FILE = "cmartires_privateKey.PEM"
+
+def create_self_signed_cert(cert_dir="."):
+    C_F = join(cert_dir, CERT_FILE)
+    K_F = join(cert_dir, KEY_FILE)
+
+    if not exists(C_F) or not exists(K_F):
+        k = crypto.PKey()
+        k.generate_key(crypto.TYPE_RSA, 2048)
+        cert = crypto.X509()
+        cert.get_subject().C = "United States"
+        cert.get_subject().ST = "NV"
+        cert.get_subject().L = "Reno"
+        cert.get_subject().O = "University of Nevada, Reno"
+        cert.get_subject().OU = "CSE"
+        cert.get_subject().CN = gethostname()
+        cert.set_serial_number(42)
+        cert.gmtime_adj_notBefore(0)
+        cert.gmtime_adj_notAfter(156780000)
+        cert.set_issuer(cert.get_subject())
+        cert.set_pubkey(k)
+        cert.sign(k, 'sha512')
+        open(C_F, "wt").write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+        open(K_F, "wt").write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k))
+
+        os.system("openssl rsa -in cmartires_privateKey.PEM -pubout -out cmartires_publicKey.key")
+
 #modify this code so that it also generates self signed certificate and keys
